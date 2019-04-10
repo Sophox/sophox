f52e96beea45a7be8beb7003129ce38db4aefd57
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2019-04-10 01:00:04 -0400

    fix metabot docker runner

--- metabot/run-docker.sh
+++ metabot/run-docker.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+IMAGE=sophox/metabot:latest
+
 docker stop metabot ; docker rm metabot ; :
 
+docker pull ${IMAGE}
+
 docker run \
+    --detach \
     --name metabot \
     --restart on-failure \
     -e "YURIKBOT_PASSWORD=$(<./password)" \
     -v $PWD/_cache/:/usr/src/app/_cache \
-    sophox/metabot:latest
+    ${IMAGE}
